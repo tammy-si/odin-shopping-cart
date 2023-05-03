660434b4e407804e5bcced4c56d119e1e26460bb
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-05-02 23:31:03 -0700

    Added scuffed loading thing

--- src/components/itemDetails.js
+++ src/components/itemDetails.js
@@ -1,12 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
+import { Bars } from 'react-loading-icons'
+
 
 function ItemDetails() {
   const [item, setItem] = useState({})
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
     fetchItem();
+    setTimeout(() => setLoading(false), 350);
   }, []);
 
       // fetch certain item from fake store api
@@ -16,17 +20,21 @@ function ItemDetails() {
     .then(json=> {setItem(json)})
   }
 
-  return (
-    <div className="item-details">
-      <img src={item.image} alt={item.title}></img>
-      <div class="info">
-        <h1>{ item.title }</h1>
-        <p>${ item.price }</p>
-        <p>{ item.description}</p>
-        <button>Add to cart</button>
+  if (loading) {
+    return <Bars className='loading' />
+  } else {
+    return (
+      <div className="item-details">
+        <img src={item.image} alt={item.title}></img>
+        <div className="info">
+          <h1>{ item.title }</h1>
+          <p>${ item.price }</p>
+          <p>{ item.description}</p>
+          <button>Add to cart</button>
+        </div>
       </div>
-    </div>
-  );
+    );  
+  }
 }
 
 export default ItemDetails;
--- src/components/shopping.js
+++ src/components/shopping.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import Card from './card';
+import { Bars } from 'react-loading-icons'
 
 function Shopping() {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   useEffect(() => {
     fetchItems();
+    setTimeout(() => setLoading(false), 200);
   },[]);
 
   // fetch from fake store api
@@ -13,18 +16,22 @@ function Shopping() {
     fetch('https://fakestoreapi.com/products')
     .then(res=>res.json())
     .then(json=> {setItems(json)})
-}
+  }
 
-  return (
-    <div className="shopping">
-      <h1>Shopping</h1>
-      <div className='items'>
-      {items.map((item) => {
-        return <Card item = {item}/>
-      })}
+  if (loading) {
+    return <Bars className='loading' />
+  } else {
+      return (
+      <div className="shopping">
+        <h1>Shopping</h1>
+        <div className='items'>
+        {items.map((item) => {
+          return <Card item = {item}/>
+        })}
+        </div>
       </div>
-    </div>
-  );
+      );
+  }
 }
 
 export default Shopping;
