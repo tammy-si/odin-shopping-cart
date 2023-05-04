f04a400a3a0e3c4a42fc0e74a6de5f973429981b
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-05-03 20:41:14 -0700

    Added some formatting for the money

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 function Card({item}) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
   return (
     <Link to={`/shop/${ item.id }`}>
         <div className="item" key={item.id}>
             <img src={item.image} alt={item.title}></img>
             <p>{item.title}</p>
-            <p>${item.price}</p>
+            <p>{formatter.format(item.price)}</p>
         </div>
     </Link>
   );
--- src/components/cart.js
+++ src/components/cart.js
@@ -3,32 +3,32 @@ import Card from './card';
 import { Bars } from 'react-loading-icons'
 
 function Cart({cartItems, increaseQuantity, decreaseQuantity}) {
-    
-  const [loading, setLoading] = useState(true);
-    console.log(cartItems)
-//   if (loading) {
-//     return <Bars className='loading' />
-//   } else {
-      return (
-      <div className="shopping">
-        <h1>Cart</h1>
-        <div className='items'>
-            {cartItems.map(item => {
-                return (
-                <div className='cart-item' key={item.item.id}>
-                    <p>{item.item.title}</p>
-                    <div className="quantity">
-                        <button onClick={()=> decreaseQuantity(item)}>-</button>
-                        <p>{item.quantity}</p>
-                        <button onClick={()=>increaseQuantity(item)}>+</button>
-                    </div>
-                </div>
-                )
-            })}
-        </div>
-      </div>
-      );
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 
+    return (
+    <div className="shopping">
+    <h1>Cart</h1>
+    <div className='items'>
+        {cartItems.map(i => {
+            return (
+            <div className='cart-item' key={i.item.id}>
+                <img src={i.item.image} alt={i.item.title}></img>
+                <p>{i.item.title}</p>
+                <p>{formatter.format(i.item.price)}</p>
+                <div className="quantity">
+                    <button onClick={()=> decreaseQuantity(i)}>-</button>
+                    <p>{i.quantity}</p>
+                    <button onClick={()=>increaseQuantity(i)}>+</button>
+                </div>
+            </div>
+            )
+        })}
+    </div>
+    </div>
+    );
 }
 
 export default Cart;
